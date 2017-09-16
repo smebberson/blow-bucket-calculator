5cc4636f2f59192069451b013c7084946bf949f2
Scott Mebberson <scott@scottmebberson.com>
2017-09-16 15:33:45 +0930

    Basic working version of the calculator.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+
 // Node modules.
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
 import '../node_modules/daemonite-material/js/material.js';
-import '../node_modules/daemonite-material/css/material.css';
 
 // CSS.
 import './scss/index.scss';

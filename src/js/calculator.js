2ba39f7f53b7e8b1fbc07d3e7b97d6a22199ac53
Scott Mebberson <scott@scottmebberson.com>
2017-09-16 16:32:46 +0930

    Storing the income in a cookie, so you can return later.

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,5 +1,10 @@
 'use strict';
 
+import Cookie from 'js-cookie';
+
+// Store the Cookie class for later use.
+window.Cookie = Cookie;
+
 var calculator = require('../../calculator.marko');
 
 calculator.renderSync().appendTo(document.body);
